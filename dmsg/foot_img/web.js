8f050116ba30b63fc76e5210572d27491aea4985
YYrainbow <126034919+YYrainbow@users.noreply.github.com>
2023-12-28 22:46:19 +0800

    myweb
    myweb

---
+++ dmsg/foot_img/web.js
@@ -0,0 +1,13 @@
+var myVid=document.getElementById("Music");
+
+//
+
+function ToggleMusic(){
+	if(myVid.paused){
+		$("#MusicToggleButton").html('暂停');
+		myVid.play();
+	}else{
+		$("#MusicToggleButton").html('播放');
+		myVid.pause();
+	}
+}
